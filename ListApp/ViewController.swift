52c2a252c18f1b0f38b9da63524e8c720bb353eb
olgamart <olgaprim@yandex.ru>
2017-12-04 13:05:05 +0300

    .becomeFirstResponder() in viewDidAppear

--- ListApp/ViewController.swift
+++ ListApp/ViewController.swift
@@ -18,13 +18,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameField.becomeFirstResponder()
+ //       nameField.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        nameField.becomeFirstResponder()
     }
 
     @IBAction func add(_ sender: UIButton) {
